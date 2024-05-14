7ca59009548e09f62597caeaa13067333c04b8d3
Ahmed Abd El-Ghany <ahmedabdoelhewala1211@gmail.com>
2024-05-15 00:01:24 +0300

    parteners section

--- js/Custom.js
+++ js/Custom.js
@@ -36,4 +36,26 @@ $(document).ready(function () {
       onlyInViewport: true,
     },
   });
+
+  const partenerSwiper = new Swiper(".partnersSlider", {
+    slidesPerView: 5,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      "@0.00": {
+        slidesPerView: 1,
+      },
+      "@0.75": {
+        slidesPerView: 2,
+      },
+      "@1.00": {
+        slidesPerView: 3,
+      },
+      "@1.50": {
+        slidesPerView: 4,
+      },
+    },
+  });
 });

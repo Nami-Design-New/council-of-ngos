662047b8e05c2e74cf28c2e4ba6fe4d14fbfb509
Ahmed Abd El-Ghany <ahmedabdoelhewala1211@gmail.com>
2024-05-20 11:51:19 +0300

    Register & Dashboard

--- js/Custom.js
+++ js/Custom.js
@@ -39,6 +39,15 @@ $(document).ready(function () {
   const registerForm = document.querySelectorAll(
     `.auth-page .auth-container .auth-form-container .auth-form`
   );
+  const orgEditWizardTags = document.querySelectorAll(
+    `.org-edit-page .org-edit-section-container .org-edit-section-content .org-edit-wizard .wizard-tag`
+  );
+  const registerWizardTags = document.querySelectorAll(
+    `.register-page .auth-container .auth-form-container .register-form .register-wizard .wizard-tag`
+  );
+  const orgRegisterWizardTags = document.querySelectorAll(
+    `.org-register-page .org-register-container .org-register-form .register-wizard .wizard-tag`
+  );
 
   // toggle opening the nav menu
   menuIconBox?.addEventListener("click", function () {
@@ -135,6 +144,41 @@ $(document).ready(function () {
     }
   });
 
+  registerWizardTags.forEach((tag) => {
+    tag.addEventListener("click", function () {
+      $(
+        ".register-page .auth-container .auth-form-container .register-form"
+      )?.hide();
+      $(".register-page #" + tag.getAttribute("data-wizard")).css(
+        "display",
+        "flex"
+      );
+      currentRegisterFormWizard = +tag
+        .getAttribute("data-wizard")
+        .split("-")[1];
+    });
+  });
+
+  // org edit wizard navigation
+  $(
+    `.org-edit-page .org-edit-section-container .org-edit-section-content`
+  ).hide();
+  $(
+    `.org-edit-page .org-edit-section-container .org-edit-section-content#form-1`
+  ).css("display", "flex");
+  orgEditWizardTags.forEach((tag) => {
+    tag.addEventListener("click", function () {
+      $(
+        `.org-edit-page .org-edit-section-container .org-edit-section-content`
+      ).hide();
+      $(
+        `.org-edit-page .org-edit-section-container .org-edit-section-content#${tag.getAttribute(
+          "data-wizard"
+        )}`
+      ).css("display", "flex");
+    });
+  });
+
   // org register form wizards
   let currentORGRegisterFormWizard = 1;
   $(".org-register-page .org-register-container .org-register-form").hide();
@@ -172,6 +216,21 @@ $(document).ready(function () {
       currentORGRegisterFormWizard--;
       $(".org-register-page #form-" + currentORGRegisterFormWizard).show();
     }
+  });
+
+  orgRegisterWizardTags.forEach((tag) => {
+    tag.addEventListener("click", function () {
+      $(
+        ".org-register-page .org-register-container .org-register-form"
+      )?.hide();
+      $(".org-register-page #" + tag.getAttribute("data-wizard")).css(
+        "display",
+        "flex"
+      );
+      currentORGRegisterFormWizard = +tag
+        .getAttribute("data-wizard")
+        .split("-")[1];
+    });
   });
 
   const mainSlider = new Swiper(".mainSlider", {
@@ -251,3 +310,202 @@ $(document).ready(function () {
     },
   });
 });
+
+// Add measurment chart
+let measurmentOptions = {
+  series: [
+    {
+      name: "مستوى التطبيق",
+      data: [31, 40, 28, 51, 42, 90, 100],
+    },
+    {
+      name: "مستوى الفاعلية",
+      data: [11, 32, 45, 32, 34, 52, 41],
+    },
+    {
+      name: "مستوى الوجود",
+      data: [15, 11, 32, 18, 9, 24, 11],
+    },
+  ],
+  chart: {
+    height: 350,
+    width: "100%",
+    type: "area",
+    animations: {
+      enabled: true,
+      easing: "easeinout",
+      speed: 1500,
+      animateGradually: {
+        enabled: true,
+        delay: 300,
+      },
+      dynamicAnimation: {
+        enabled: true,
+        speed: 1000,
+      },
+    },
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  stroke: {
+    curve: "smooth",
+  },
+  xaxis: {
+    type: "category",
+    categories: [
+      "البنية التحتية و التقنية",
+      "التفعيل المجتمعي والتطوع",
+      "إدارة العلاقات والاتصال المؤسسي",
+      "إدارة الموارد البشرية",
+      "الاستدامة المالية",
+      "التنظيم المالي",
+      "إدارة المشاريع والعمليات",
+      "حوكمة المنظمة وإدارة المخاطر والالتزام",
+      "إدارة الاستراتيجية والأداء",
+    ],
+  },
+  yaxis: {
+    opposite: true,
+    labels: {
+      formatter: function (value) {
+        return value + "%";
+      },
+    },
+  },
+  tooltip: {
+    y: {
+      formatter: function (val) {
+        return val + "%";
+      },
+    },
+  },
+  colors: ["#F9C74F", "#2D9CDB", "#F94144"],
+  fill: {
+    opacity: 0.8,
+    type: "gradient",
+    gradient: {
+      shade: "light",
+      type: "horizontal",
+      shadeIntensity: 0.25,
+      gradientToColors: undefined,
+      inverseColors: true,
+      opacityFrom: 0.75,
+      opacityTo: 0.75,
+      stops: [0, 100],
+    },
+  },
+  legend: {
+    position: "top",
+    horizontalAlign: "center",
+    floating: true,
+    offsetY: -25,
+    offsetX: -5,
+  },
+};
+
+let measurmentChart = new ApexCharts(
+  document.querySelector("#measurment-chart"),
+  measurmentOptions
+);
+measurmentChart.render();
+
+// Add supply & demands chart
+let supplyDemandsOptions = {
+  series: [
+    {
+      data: ["55%", "22%", "40%", "84%", "60%", "28%", "13%", "75%", "80%"],
+    },
+  ],
+  chart: {
+    height: 350,
+    type: "bar",
+  },
+  colors: ["#1D968F"],
+  plotOptions: {
+    bar: {
+      columnWidth: "15px",
+      distributed: true,
+    },
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  legend: {
+    show: false,
+  },
+  xaxis: {
+    categories: [
+      ["إدارة الاستراتيجية والأداء"],
+      ["حوكمة المنظمة وإدارة المخاطر والالتزام"],
+      ["التنظيم المالي"],
+      ["إدارة العلاقات والاتصال المؤسسي"],
+      ["التفعيل المجتمعي والتطوع"],
+      ["إدارة المشاريع والعمليات"],
+      ["البنية التحتية و التقنية"],
+      ["الاستدامة المالية"],
+      ["إدارة الموارد البشرية"],
+    ],
+    labels: {
+      style: {
+        colors: ["#666B88"],
+        fontSize: "13px",
+        fontFamily: "MadaniArabic-Regular",
+        fontWeight: "400",
+      },
+    },
+  },
+};
+
+let supplyDemandsChart = new ApexCharts(
+  document.querySelector("#supply-demands-chart"),
+  supplyDemandsOptions
+);
+supplyDemandsChart.render();
+
+const levelFormCheckInputs = document.querySelectorAll(
+  `.level-statistics-page .level-container .level-statistics-form .form-box .form-box-item .input-group input`
+);
+
+function updateQualifiedInputs() {
+  levelFormCheckInputs?.forEach((input) => {
+    if (
+      input.parentNode.parentNode.parentElement.classList.contains("qualified")
+    ) {
+      if (input.parentNode.classList.contains("qualified-input")) {
+        input.checked = true;
+      } else if (input.parentNode.classList.contains("not-qualified-input")) {
+        input.checked = false;
+      }
+    } else if (
+      input.parentNode.parentNode.parentElement.classList.contains(
+        "not-qualified"
+      )
+    ) {
+      if (input.parentNode.classList.contains("qualified-input")) {
+        input.checked = false;
+      } else if (input.parentNode.classList.contains("not-qualified-input")) {
+        input.checked = true;
+      }
+    }
+  });
+}
+
+const levelFormBtnsContainerItems = document.querySelectorAll(
+  `.level-statistics-page .level-container .level-statistics-form .form-box .form-box-item .item-btns-container`
+);
+levelFormBtnsContainerItems?.forEach((item) => {
+  item.addEventListener("click", function () {
+    if (item.parentElement.classList.contains("not-qualified")) {
+      item.parentElement.classList.remove("not-qualified");
+      item.parentElement.classList.add("qualified");
+      updateQualifiedInputs();
+    } else if (item.parentElement.classList.contains("qualified")) {
+      item.parentElement.classList.remove("qualified");
+      item.parentElement.classList.add("not-qualified");
+      updateQualifiedInputs();
+    }
+  });
+});
+
+updateQualifiedInputs();
